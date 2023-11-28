cf0a6b96795e4cea766aea13fef9123ed1c3ef57
hellemmourap <hellem.moura79@gmail.com>
2023-11-28 00:42:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num, num2, opcao, result = 0;
+            Console.WriteLine("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Selecione uma operação: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            opcao = Convert.ToInt32(Console.ReadLine());
+            
+            switch(opcao)
+            {
+                case 1: 
+                    result = num + num2;
+                    break;
+                case 2:
+                    result = num - num2;
+                    break;
+                case 3:
+                    result = num * num2;
+                    break;
+                case 4:
+                    result = num / num2;
+                    break;
+                default:
+                    Console.WriteLine("Option not found!!!");
+                    break;
+            }
+            Console.WriteLine("Resultado = {0}", result);
+        }
+    }
+}
